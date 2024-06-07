ae801478b1656a1d343585f1feee59ed555c4c22
Luxa <Skofild2228@mail.ru>
2024-06-08 00:14:19 +0600

    HomeApi2

--- app/src/main/java/com/example/smartlab/Katalog.java
+++ app/src/main/java/com/example/smartlab/Katalog.java
@@ -27,7 +27,7 @@ private List<CatalogItem> catalogItems;
 
     @SuppressLint("MissingInflatedId")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {g
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_katalog);
         catalogList = findViewById(R.id.catalog_list);
